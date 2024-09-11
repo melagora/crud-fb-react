ef489a58221d9c6edf80477571b6a9e417df3fc8
Melvin González <54681730+melagora@users.noreply.github.com>
2024-09-11 02:05:06 -0600

    Funcionalidad para el boton "editar"

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,6 +1,75 @@
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router"
+import { getDoc, updateDoc, doc } from "firebase/firestore"
+import { db } from "../firebaseConfig/firebase"
+
 const Edit = () => {
+  const [description, setDescription] = useState(' ');
+  const [stock, setStock] = useState(0);
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const update = async (e) => {
+    e.preventDefault();
+    const product = doc(db, "products", id)
+    const data = { description: description, stock: stock }
+    await updateDoc(product, data)
+    navigate('/')
+  }
+
+  const getProductById = async (id) => {
+    const product = await getDoc(doc(db, "products", id))
+    if (product.exists()) {
+      //console.log(product.data)
+      setDescription(product.data().description)
+      setStock(product.data().stock)
+    } else {
+      console.log("El producto no existe")
+    }
+  }
+
+  useEffect(() => {
+    getProductById(id)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+
   return (
-    <div>Edit</div>
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <h1>Edit Product</h1>
+
+          <form onSubmit={update}>
+
+            <div className='mb-3'>
+              <label className='form-label'>Description</label>
+              <input
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                type="text"
+                className='form-control'
+                required
+                placeholder="Ingrese la descripción del producto"
+              />
+            </div>
+
+            <div className='mb-3'>
+              <label className='form-label'>Guardar cambios del producto</label>
+              <input
+                value={stock}
+                onChange={(e) => setStock(e.target.value)}
+                type="number"
+                className='form-control'
+              />
+            </div>
+
+            <button type='submit' className='btn btn-primary'>Guardar Producto</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
